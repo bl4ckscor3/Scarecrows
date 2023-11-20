7b87a417abab3f094bfa44a9efbe19978e1348b7
bl4ckscor3 <bl4ckscor33@gmail.com>
2023-11-20 15:52:36 +0100

    fix compatibility with NeoForge 20.2.59-beta

--- src/main/java/bl4ckscor3/mod/scarecrows/Scarecrows.java
+++ src/main/java/bl4ckscor3/mod/scarecrows/Scarecrows.java
@@ -3,12 +3,12 @@ package bl4ckscor3.mod.scarecrows;
 import bl4ckscor3.mod.scarecrows.block.ArmBlock;
 import bl4ckscor3.mod.scarecrows.entity.Scarecrow;
 import bl4ckscor3.mod.scarecrows.type.ScarecrowType;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializer;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
 import net.minecraft.world.phys.AABB;
@@ -16,25 +16,25 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
-import net.neoforged.neoforge.registries.ForgeRegistries;
-import net.neoforged.neoforge.registries.ForgeRegistries.Keys;
-import net.neoforged.neoforge.registries.RegistryObject;
+import net.neoforged.neoforge.registries.NeoForgeRegistries.Keys;
 
 @Mod(Scarecrows.MODID)
 public class Scarecrows {
 	public static final String MODID = "scarecrows";
 	public static final String PREFIX = MODID + ":";
-	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
-	public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, MODID);
+	public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
+	public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(Registries.ENTITY_TYPE, MODID);
 	public static final DeferredRegister<EntityDataSerializer<?>> ENTITY_DATA_SERIALIZERS = DeferredRegister.create(Keys.ENTITY_DATA_SERIALIZERS, MODID);
-	public static final RegistryObject<ArmBlock> ARM = BLOCKS.register("arm", () -> new ArmBlock(Properties.of()
+	public static final DeferredBlock<ArmBlock> ARM = BLOCKS.register("arm", () -> new ArmBlock(Properties.of()
 	//@formatter:off
 			.strength(0.25F, 1.0F)
 			.sound(SoundType.WOOD)
 			.isRedstoneConductor((state, world, pos) -> false)));
 	//@formatter:on
-	public static final RegistryObject<EntityType<Scarecrow>> SCARECROW_ENTITY_TYPE = ENTITY_TYPES.register("scarecrow", () -> EntityType.Builder.<Scarecrow>of(Scarecrow::new, MobCategory.MISC)
+	public static final DeferredHolder<EntityType<?>, EntityType<Scarecrow>> SCARECROW_ENTITY_TYPE = ENTITY_TYPES.register("scarecrow", () -> EntityType.Builder.<Scarecrow>of(Scarecrow::new, MobCategory.MISC)
 	//@formatter:off
 			.sized(1.0F, 1.0F)
 			.setTrackingRange(256)
@@ -42,7 +42,7 @@ public class Scarecrows {
 			.setShouldReceiveVelocityUpdates(false)
 			.build(PREFIX + "scarecrow"));
 	//@formatter:on
-	public static final RegistryObject<EntityDataSerializer<ScarecrowType>> SCARECROW_ENTITY_DATA_SERIALIZER = ENTITY_DATA_SERIALIZERS.<EntityDataSerializer<ScarecrowType>>register("scarecrow_type", () -> new EntityDataSerializer<>() {
+	public static final DeferredHolder<EntityDataSerializer<?>, EntityDataSerializer<ScarecrowType>> SCARECROW_ENTITY_DATA_SERIALIZER = ENTITY_DATA_SERIALIZERS.<EntityDataSerializer<ScarecrowType>>register("scarecrow_type", () -> new EntityDataSerializer<>() {
 		@Override
 		public void write(FriendlyByteBuf buf, ScarecrowType value) {
 			buf.writeUtf(value.getName());
@@ -70,7 +70,7 @@ public class Scarecrows {
 			return value;
 		}
 	});
-	public static final RegistryObject<EntityDataSerializer<AABB>> AABB_ENTITY_DATA_SERIALIZER = ENTITY_DATA_SERIALIZERS.<EntityDataSerializer<AABB>>register("aabb", () -> new EntityDataSerializer<>() {
+	public static final DeferredHolder<EntityDataSerializer<?>, EntityDataSerializer<AABB>> AABB_ENTITY_DATA_SERIALIZER = ENTITY_DATA_SERIALIZERS.<EntityDataSerializer<AABB>>register("aabb", () -> new EntityDataSerializer<>() {
 		@Override
 		public void write(FriendlyByteBuf buf, AABB value) {
 			buf.writeDouble(value.minX);
